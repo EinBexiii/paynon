36ba08679bba70031cac42a05958f81acf962d92
Erik Miller <erikmiller04@gmail.com>
2022-05-07 19:15:01 +0200

    add IOS google login support

--- paynon/lib/utils/google/google_constant.dart
+++ paynon/lib/utils/google/google_constant.dart
@@ -1,5 +1,7 @@
+import 'dart:io';
+
 import 'package:google_sign_in/google_sign_in.dart';
 
 class GoogleConstant {
-  static GoogleSignIn googleSignIn = GoogleSignIn(clientId: "406606233683-tch1q93ogb9f811ncqf4q44ia257jmri.apps.googleusercontent.com");
+  static GoogleSignIn googleSignIn = GoogleSignIn(clientId: Platform.isIOS ? "406606233683-aba99r480m677ile4rebo8fn64bfht0h.apps.googleusercontent.com" : "406606233683-tch1q93ogb9f811ncqf4q44ia257jmri.apps.googleusercontent.com");
 }
